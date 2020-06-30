16adbc796af0cfb64da155e79fc78be85836f755
Arthur (antimech) <arthur.antimech@pm.me>
2020-06-30 14:50:16 +0600

    Add custom alias generation to Link factory

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -7,10 +7,13 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Str;
 
 $factory->define(Link::class, function (Faker $faker) {
+    $customOrRandom = random_int(0, 1);
     $dateOrNull = random_int(0, 1);
 
     return [
-        'hash' => Str::random(8),
+        'hash' => $customOrRandom
+            ? $faker->userName . $faker->numberBetween(1, 100)
+            : Str::random(8),
         'link' => 'https://source.unsplash.com/collection/' . random_int(1, 50),
         'expired_at' => $dateOrNull ? $faker->date() : null
     ];
