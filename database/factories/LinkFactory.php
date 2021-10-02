e5b3901beb5375255bd187447282e2dbc2e5706d
Arthur (antimech) <arthur.antimech@pm.me>
2021-10-03 03:07:04 +0600

    Add date range to expired_at field in LinkFactory

--- database/factories/LinkFactory.php
+++ database/factories/LinkFactory.php
@@ -15,6 +15,8 @@ $factory->define(Link::class, function (Faker $faker) {
             ? $faker->userName . $faker->numberBetween(1, 100)
             : Str::random(8),
         'link' => 'https://source.unsplash.com/collection/' . random_int(1, 50),
-        'expired_at' => $dateOrNull ? $faker->date() : null
+        'expired_at' => $dateOrNull
+            ? $faker->dateTimeBetween('-1 week', '+ 1 week')->format('Y-m-d')
+            : null
     ];
 });
